e1e1d0ef12b7a438b1535ce2036b57970d650b27
Novanef <trios.timothe@hotmail.fr>
2021-02-25 21:54:11 +0100

    tentative

--- Assets/RayCastTest.cs
+++ Assets/RayCastTest.cs
@@ -7,11 +7,17 @@ public class RayCastTest : MonoBehaviour
     public string la_main;
     private float timer;
     private float ping;
+    public LayerMask[] layers;
+    public string[] names;
+    public GameObject[] meubles;
+    private GameObject meuble;
+    private bool est_tenu;
     // Start is called before the first frame update
     void Start()
     {
         timer = 0f;
         ping = 2f;
+        est_tenu = false;
     }
 
     // Update is called once per frame
@@ -26,5 +32,30 @@ public class RayCastTest : MonoBehaviour
             timer = 0f;
         }
         timer += Time.deltaTime;
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) && la_main == "La main droite")
+        {
+            if (Physics.Raycast(transform.position,transform.forward,out hit))
+            {
+                Debug.Log("bouton poussé, raycast a touché");
+                int index = 0;
+                foreach(string name in names)
+                {
+                    if (name == hit.transform.gameObject.name) {
+                        meuble = Instantiate(meubles[index],transform.position+transform.forward,Quaternion.identity);
+                        est_tenu = true;
+                        break;
+                    }
+                    index++;
+                }
+            }
+        }
+        if (est_tenu)
+        {
+            meuble.transform.position = transform.position + transform.forward;
+        }
+        if (OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger) && la_main == "La main droite")
+        {
+            est_tenu = false;
+        }
     }
 }
