ff24f40775337415f3d67b9c85a53beea75ea158
Novanef <trios.timothe@hotmail.fr>
2021-02-25 19:41:44 +0100

    raycast

---
+++ Assets/RayCastTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastTest : MonoBehaviour
+{
+    public string la_main;
+    private float timer;
+    private float ping;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0f;
+        ping = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (timer > ping) {
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                Debug.Log(la_main + " hit objet en " +hit.transform.position);
+        }
+            timer = 0f;
+        }
+        timer += Time.deltaTime;
+    }
+}
