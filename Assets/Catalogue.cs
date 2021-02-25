68040369afdc1a593d6e99fd6ee5cade8e910887
Novanef <trios.timothe@hotmail.fr>
2021-02-25 20:36:11 +0100

    affichage catalogue
    normalement en appuyant sur le clic gauche ou n'importe quel bouton des manettes on affiche le catalogue

---
+++ Assets/Catalogue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catalogue : MonoBehaviour
+{
+    public GameObject catalogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        catalogue.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            catalogue.SetActive(!catalogue.active);
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Any))
+        {
+            catalogue.SetActive(!catalogue.active);
+        }
+    }
+}
