2e5f3bcd1cb98c62a8faabb7da036c18f01247da
Novanef <trios.timothe@hotmail.fr>
2021-03-03 23:35:55 +0100

    ajout rotation + déplacement + console de débug + détection des obstacles

--- Assets/Meuble.cs
+++ Assets/Meuble.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class Meuble : MonoBehaviour
 {
-    public bool collision;
+    public bool can_place;
     private List<Color> colors = new List<Color>();
     // Start is called before the first frame update
     void Start()
     {
- 
+        can_place = true;
+        foreach (Material material in gameObject.GetComponent<MeshRenderer>().materials)
+        {
+            colors.Add(material.color);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        can_place = false;
+        Debug.Log("collision");
+        foreach (Material material in gameObject.GetComponent<MeshRenderer>().materials)
+        {
+            material.color = Color.red;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        can_place = true;
+        Debug.Log("collision sortie");
+        int index = 0;
+        foreach (Material material in gameObject.GetComponent<MeshRenderer>().materials)
+        {
+            material.color = colors[index];
+            index++;
+        }
     }
 }
--- Assets/RayCastTest.cs
+++ Assets/RayCastTest.cs
@@ -1,32 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class RayCastTest : MonoBehaviour
 {
-    public string la_main;
     public LayerMask[] layers;
     public string[] names;
     public GameObject[] meubles;
     private GameObject meuble;
     private bool est_tenu;
+    public Text text;
+    public OVRPlayerController controller;
+    private float rotation;
     // Start is called before the first frame update
     void Start()
     {
         est_tenu = false;
+        rotation = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
-        if ((OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || Input.GetMouseButtonDown(0)) && la_main == "La main droite")
+        if ((OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || Input.GetMouseButtonDown(0)))
         {
-            if (est_tenu && !meuble.GetComponent<Meuble>().collision)
+            if (est_tenu && meuble.GetComponent<Meuble>().can_place)
             {
                 est_tenu = false;
+                meuble.GetComponent<Rigidbody>().useGravity = true;
+                meuble.transform.parent = null;
+                meuble.GetComponent<Collider>().isTrigger = false;
 
-                meuble.AddComponent<Rigidbody>();
+                controller.EnableRotation = true;
+                rotation = 0.0f;
 
             }
             else
@@ -39,6 +46,9 @@ public class RayCastTest : MonoBehaviour
                         if (name == hit.transform.gameObject.name)
                         {
                             meuble = Instantiate(meubles[index], transform.position + transform.forward * 0.5f, Quaternion.identity);
+
+                            meuble.AddComponent<Rigidbody>().useGravity = false;
+                            meuble.transform.parent = gameObject.transform;
                             est_tenu = true;
                             break;
                         }
@@ -49,9 +59,46 @@ public class RayCastTest : MonoBehaviour
         }
         if (est_tenu)
         {
-            meuble.transform.position = transform.position + transform.forward;
+            meuble.transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+            Vector2 secondaryAxis = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+            rotation += secondaryAxis.x;
+            Clear();
+            Print("test\n");
+            Print(secondaryAxis.ToString()+"\n");
+            Print(rotation.ToString()+"\n");
+            Print(OVRInput.Get(OVRInput.Button.Any).ToString()+"\n");
+            controller.EnableRotation = false;
+            if ( OVRInput.Get(OVRInput.Button.Two))
+            {
+                Print("B pressé\n");
+                meuble.transform.position += transform.forward / 50;
+            }
+            
+            if ( OVRInput.Get(OVRInput.Button.One))
+            {
+                Print("A pressé\n");
+                meuble.transform.position -= transform.forward / 50;
+            }
+            
+            if ((meuble.transform.position - transform.position).magnitude > 1f)
+            {
+                meuble.transform.position = transform.position + transform.forward;
+            }
+            if ((meuble.transform.position - transform.position).magnitude < 0.2f)
+            {
+                meuble.transform.position = transform.position + transform.forward * 0.2f;
+            }
+            meuble.transform.Rotate(0, rotation, 0);
         }
     }
-    
+    void Print(string s)
+    {
+        text.text += s;
+    }
+
+    void Clear()
+    {
+        text.text = "";
+    }
 
 }
