d0a4a2f1b8a916b349cf744a6bcec2e5e3391664
Novanef <trios.timothe@hotmail.fr>
2021-03-20 12:34:29 +0100

    changement aides

--- Assets/ResetButton.cs
+++ Assets/ResetButton.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class ResetButton : MonoBehaviour
 {
-    private SceneManager mangager;
     // Start is called before the first frame update
     void Start()
     {
